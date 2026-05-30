28b652c8b65e3e03d6081a2e5258ef1800486713
shahriarpro74-hub <shahriarpro74@gmail.com>
2026-05-30 12:11:21 +0600

    Add files via upload

---
+++ src/pages/Index.tsx
@@ -0,0 +1,27 @@
+import Navbar from "@/components/Navbar";
+import HeroSection from "@/components/HeroSection";
+import AboutSection from "@/components/AboutSection";
+import StatsSection from "@/components/StatsSection";
+import ServicesSection from "@/components/ServicesSection";
+import PortfolioSection from "@/components/PortfolioSection";
+import TestimonialsSection from "@/components/TestimonialsSection";
+import ContactSection from "@/components/ContactSection";
+import Footer from "@/components/Footer";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-background overflow-x-hidden">
+      <Navbar />
+      <HeroSection />
+      <PortfolioSection />
+      <AboutSection />
+      <StatsSection />
+      <ServicesSection />
+      <TestimonialsSection />
+      <ContactSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default Index;
